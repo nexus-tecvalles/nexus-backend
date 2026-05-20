5553ff21ec97d86af1e807564846bc8779f96405
Espuna21 <b24690658@tecvalles.mx>
2026-05-20 10:40:41 -0600

    update db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://user:password@localhost:5432/project_management")
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://user:Xzp0gt8fZdzq1z3K0WiqJHdMJDc0YrNo@dpg-d86u1hojo6nc73esd8jg-a/project_management_zx6h")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
